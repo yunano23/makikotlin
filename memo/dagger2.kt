3e863a9978d63afbc462cb1828848a9c59e83d03
yunano23 <mama.yunano@gmail.com>
2019-08-08 06:58:30 +0900

    dagger

--- memo/dagger2.kt
+++ memo/dagger2.kt
@@ -1,164 +1,290 @@
 /*----------------------------------------------------------------------------------
                                         Dagger2
- ----------------------------------------------------------------------------------
- https://qiita.com/ko2ic/items/35d077499734bfd777fc
+----------------------------------------------------------------------------------
+https://qiita.com/ko2ic/items/35d077499734bfd777fc
 
- [dependency injection（DI）]
- ・外部から注入（injection）する手法
-    ・テストなど、状況に応じて注入するインスタンスを切替えられる
-    ・インスタンス化の方法が変わっても利用側は影響をうけない
-
+[dependency injection（DI）]
+・外部から注入（injection）する手法
+・テストなど、状況に応じて注入するインスタンスを切替えられる
+・インスタンス化の方法が変わっても利用側は影響をうけない
+----------------------------------------------------------------------------------
 [@Component]
- ・injectorのこと
- ・inject される側（Activity）は、Component を経由して必要なインスタンを注入してもらう
- ・@Component つけて、abstractクラス、 interface で定義
- ・modules 属性に必要な Moduleクラスを指定
- ・インターフェース型の変数に、インジェクトをしたい場合
-   Moduleを切替えることで、生成するインスタンスを指定できる
- ・ビルドすると Component の実装クラスが自動生成される。名前は「 Dagger + Componentクラス名」
+・injectorのこと
+・inject される側（Activity）は、Component を経由して必要なインスタンを注入してもらう
+・@Component つけて、abstractクラス、 interface で定義
+・modules 属性に必要な Moduleクラスを指定
+・インターフェース型の変数に、インジェクトをしたい場合
+Moduleを切替えることで、生成するインスタンスを指定できる
+・ビルドすると Component の実装クラスが自動生成される。名前は「 Dagger + Componentクラス名」
 
-    @Component(modules = ApiModule.class)
-    interface AppComponent {
-        var apiService():ApiService
-    }
+1.@Component の抽象クラスを定義
+@Component(modules = ApiModule.class)
+interface AppComponent {
+    var apiService():ApiService
+}
 
 
 
- ・Component のインスタンスを生成するには、自動生成された Component実装クラス.Builder で生成
-    val appComponent = DaggerAppComponent.builder()
-                    .appModule(AppModule(this))
-                    .modelModule(ModelModule())
-                    .build()
+2.利用側で、@Component をインスタンス化
+val appComponent = DaggerAppComponent.builder()
+                .appModule(AppModule(this))
+                .modelModule(ModelModule())
+                .build()
 
-・inject()のタイミングで、@Injectを指定しているフィールドに対して、インスタンスが挿入される
+3.利用側で、@Component.inject() で
+　@Injectを指定しているフィールドに対して、インスタンスが挿入
 
-（2.11から）
-・指定したActivityに、Componentを自動生成するModuleが作れるようになった
+AndroidInjection（2.11から）-------------------------------------------------
+・指定したActivityに、@Componentを自動生成するModuleが作れるようになった
 ・@ContributesAndroidInjector で、インジェクションして欲しいActivityを指定した@Moduleを作成し
 　＠Componentにセットしておけば、そのActivityで、@Componentのインスタンスを裏で自動生成してくれる
 
-    // 1.指定したActivityにComponentを自動生成するModule作成
-    @Module
-    abstract class AndroidModule {
+// 1.指定したActivityにComponentを自動生成するModule作成
+@Module
+abstract class AndroidModule {
 
-        @ContributesAndroidInjector
-        abstract fun contributeMainActivity(): MainActivity     // Componentを生成したいActivity
-    }
+    @ContributesAndroidInjector
+    abstract fun contributeMainActivity(): MainActivity     // Componentを生成したいActivity
+}
 
-    // 2.Componentに、@Module をセット
-    @Component(modules = [AndroidModule::class])
-    interface AppComponent {...}
+// 2.Componentに、@Module をセット
+@Component(modules = [AndroidModule::class])
+interface AppComponent {...}
 
-    // 3.Applicationクラスで、HasActivityInjectorインターフェースを実装
-    class App : Application(), HasActivityInjector {
 
-    // 4.ActivityのonCreateで、inject
-    AndroidInjection.inject(this)
+// 3.Applicationクラスで、HasActivityInjectorインターフェースを実装
+class App : Application(), HasActivityInjector {
 
-    AndroidInjection.inject(this) が具体的に何をしているかというと、
-    Applicationクラスで、HasActivityInjectorが実装されていれば、
-    DispatchingAndroidInjectorを使って引数のActivityを
-    Component(これは依存を解決するためのクラス。別名ObjectGraph。)に入れています。
-    そして、Activityで@InjectしているフィールドにInjectしてくれます
 
+// 4.ActivityのonCreateで、inject
+AndroidInjection.inject(this)
+
+
+AndroidInjection.inject(this) が具体的に何をしているかというと、
+Applicationクラスで、HasActivityInjectorが実装されていれば、
+DispatchingAndroidInjector を使って引数のActivityを
+Component(これは依存を解決するためのクラス。別名ObjectGraph。)に入れています。
+そして、Activityで @InjectをつけたフィールドにInjectしてくれます
+
+
+----------------------------------------------------------------------------------
 [@Module]
- ・責務はインスタンスの生成
- ・inject するクラスを返すメソッドを定義（@Provideメソッド）
-    [@Provides]
-    ・@Providesをつける
-    ・名前は「Provide + 生成するクラス名」
-    ・戻り値に生成したいクラスを指定
+・責務はインスタンスの生成
+・inject するクラスを返すメソッドを定義（@Provideメソッド）
+[@Provides]
+・@Providesをつける
+・名前は「Provide + 生成するクラス名」
+・戻り値に生成したいクラスを指定
+[@Binds]
+・@Providesの代わりに@Bindsを利用
+・クラスとプロバイダメソッドをabstractにすること
 
-        「@Moduleクラス名 _ @Provideメソッド名 + Factory」
-        ・裏でFactory が自動生成される。
-        ・Component は、Factory を介して Module の provideメソッド を呼出し、インスタンスを生成している
+@Module
+class AppModule(val application: Application) {
 
-
-    @Module
-    class AppModule(private val application: Application) {
-
-        @Singleton                      // 何度コールしても同じSessionManagerインスタンスが挿入される
-        @Provides
-        fun provideSessionManager(): SessionManager {
-            return SessionManager()
-        }
+    @Singleton               // 何度コールしても同じSessionManagerインスタンスが挿入される
+    @Provides
+    fun provideSessionManager(): SessionManager {
+        return SessionManager()
     }
+}
+
+
+・裏でFactory が自動生成される。
+・名前は「@Moduleクラス名 _ @Provideメソッド名 + Factory」
+・Component は、Factory を介して Module の provideメソッド を呼出し、インスタンスを生成している
 
 
 
-
-[Injectされるクラス]
+----------------------------------------------------------------------------------
+[@Injectでインスタンス化されるクラス]
 ・コンストラクタに @Inject をつける
 ・Daggerは、クラスをインスタンス化する時に @Injectを使っているコンストラクタを使う
-・@Injectがついたコンストラクタがない場合、Daggerはインスタンスは作成しない。
+・@Injectがついたコンストラクタがない場合、Daggerはインスタンスは作成できない。
+
+class MyViewModel @Inject constructor(){}
 
 
-[Activity、Fragment]
+----------------------------------------------------------------------------------
+[Activity、Fragment（利用側）]
 ・挿入したいオブジェクトに、@Injectをつける
- ・inject()のタイミングで、@Injectを指定しているフィールドに対して、インスタンスが挿入される
+・inject()のタイミングで、@Injectを指定しているフィールドに対して、インスタンスが挿入される
 
-    class MyViewModel @Inject constructor(private val mPresenter: PersonPresenter)
+ @Inject
+ lateinit var personPresenter: PersonPresenter
 
-     @Inject lateinit var personPresenter: PersonPresenter
- ・
+----------------------------------------------------------------------------------
+
 []
- ・
- ・
+----------------------------------------------------------------------------------
 []
- ・
- ・
- ----------------------------------------------------------------------------------
- [seedInstance]
- AndroidInjectorのバインディンググラフで使用されるインスタンスを提供
- ----------------------------------------------------------------------------------
- [AndroidInjector]
- (ActivityまたはFragment）のメンバーインジェクションを実行。
- dagger.Subcomponentアノテーションによって実装されている
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------*/
 
- [dagger.Subcomponentアノテーション]
- dagger.Subcomponent.Builder extends AndroidInjector.Builder.
- ----------------------------------------------------------------------------------
- [Subcomponent]
- 親のComponent からバインディングを継承するサブコンポーネント。
- ----------------------------------------------------------------------------------
- [@IntoMap]
- [@MapKey]
- マップを作成するため、@Providesメソッドによって返される値に、キーを付けるアノテーション
- @MapKeyは単一のメンバーを持ち、マップキーとして使用される。
 
-    //@Mapkey として使う@アノテーションを定義（キーとして使うものを定義)
-     @MapKey
-     @interface SomeEnumKey {
-      SomeEnum value();
+/*----------------------------------------------------------------------------------
+                                AndroidInjector
+----------------------------------------------------------------------------------
+[AndroidInjector]
+Androidタイプ(ActivityやFragment）に、メンバーインジェクションを実行。
+@Subcomponentアノテーションによって実装されている
+----------------------------------------------------------------------------------
+[@Subcomponent]
+dagger.Subcomponent.Builder extends AndroidInjector.Builder.
+親の Component からバインディングを継承するサブコンポーネント。
+----------------------------------------------------------------------------------
+[seedInstance]
+AndroidInjector のバインディンググラフで使用されるインスタンスを提供
+----------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------
+[DispatchingAndroidInjector]
+Androidタイプ（Activity、Fragmentなど）のインスタンスに、メンバーインジェクションを実行
+----------------------------------------------------------------------------------
+[HasAndroidInjectorインターフェース]
+AndroidInjector を返す #androidInjector()が定義されている
+    fun androidInjector():AndroidInjector<Object>
+
+インターフェースは、それぞれAndroidタイプのInjectorを返すメソッドが定義されている
+    HasActivityInjector.activityInjector():DispatchingAndroidInjector<Activity>
+    HasFragmentInjector.fragmentInjector():DispatchingAndroidInjector<Fragment>
+----------------------------------------------------------------------------------
+[@BindsInstance ]
+インスタンスを Component 内のキーにバインドするようにするため
+Component.Builderのメソッドや Component.Factoryのパラメーターをマークするのに使う
+
+ビルダーやファクトリのクライアントが、 これらのインスタンスがコンポーネント内に渡して注入できるようにする
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------
+                        DaggerAppCompatActivity
+----------------------------------------------------------------------------------
+[DaggerAppCompatActivity]
+onCreateのタイミングで、　AndroidInjection.inject(this)　する
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        AndroidInjection.inject(this);
+        super.onCreate(savedInstanceState);
     }
+----------------------------------------------------------------------------------
+[DaggerApplication]
+・HasActivityInjector、HasFragmentInjectorインターフェースなどをimplementsしている
+    class DaggerApplication extends Application
+        implements HasActivityInjector, HasFragmentInjector,
 
+・DispatchingAndroidInjectorが、メンバーインジェクションされている
+    @Inject DispatchingAndroidInjector<Activity> activityInjector
+    @Inject DispatchingAndroidInjector<Fragment> fragmentInjector
 
-     @Module
-    class SomeModule {
-       //@Providesメソッド で返される値に、@アノテーションで key をつけて Map にする
-       @Provides
-       @IntoMap
-       @SomeEnumKey(SomeEnum.FOO)
-      fun provideFooValue():Integer {
-        return 2                   // put(SomeEnum.FOO, 2)
+・HasActivityInjector.activityInjector()で、injectされたメンバーを返すようになってる
+      @Override
+      public DispatchingAndroidInjector<Activity> activityInjector() {
+        return activityInjector;
       }
-    }
 
-    class SomeClass {
-       @Inject
-       var map: Map<SomeEnum, Integer>          //map.get(SomeEnum.FOO) == 2
-    }
+ [追加で必要な実装]
+ ・applicationInjectorを返すメソッドを実装すること
+     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+         return DaggerAppComponent.builder().create(this)
+     }
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------*/
 
- ----------------------------------------------------------------------------------
- []
- ----------------------------------------------------------------------------------
- []
- ----------------------------------------------------------------------------------
- []
- ----------------------------------------------------------------------------------
- []
- ----------------------------------------------------------------------------------
- []
- ----------------------------------------------------------------------------------
- []
- ----------------------------------------------------------------------------------*/
+/*----------------------------------------------------------------------------------
+                                 @Binds
+----------------------------------------------------------------------------------
+[違い]
+・@Provides の代わりに @Binds
+・class と Providesメソッド は、abstract であること
+・同一Module内に @Providesメソッド を定義できない
+----------------------------------------------------------------------------------
+@Provides は、Provideメソッド毎に裏で Factoryクラスをたくさんつくるが、
+@Binds は、Factoryを作らないのでコードレス化できるのでこれからはコッチが推奨
+
+abstract fun provideクラス名( some:実装クラス型):インターフェース型
+----------------------------------------------------------------------------------*/
+ interface IRepository {
+     fun fetch():Item
+}
+
+ class RepositoryImpl : IRepository {
+    override fun fetch() :Item{
+        // 何らかの実装
+    }
+}
+
+[@Bindsでない場合]
+@Module
+class Module {
+    @Provides
+    fun provideRepository( repository:RepositoryImpl) :IRepository{
+        return repository;
+    }
+}
+--------------------------------------------------------------------
+[@Bindsの場合]
+@Module
+abstract class Module {
+    @Binds
+    abstract  provideRepository(RepositoryImpl repository):IRepository
+}
+
+
+
+
+
+/*----------------------------------------------------------------------------------
+                               @IntoMap / @MapKey
+----------------------------------------------------------------------------------
+[@IntoMap]
+
+----------------------------------------------------------------------------------
+
+[@MapKey]
+Mapを作成するため、@Providesメソッドによって返される値に、キーを付けるアノテーション
+@MapKeyは単一のメンバーを持ち、マップキーとして使用される。
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------
+[]
+----------------------------------------------------------------------------------*/
+ //@MapKey として使う@アノテーションを定義（キーとして使うものを定義)
+  @MapKey
+  @interface SomeEnumKey {
+    val value:SomeEnum
+ }
+
+
+ @Module
+ class SomeModule {
+   //@Providesメソッド で返される値に、@アノテーションで key をつけて Map にする
+   @Provides
+   @IntoMap
+   @SomeEnumKey(SomeEnum.FOO)
+   fun provideFooValue():Int {
+     return 2                   // put(SomeEnum.FOO, 2)
+   }
+ }
+
+ class SomeClass {
+    @Inject
+    var map: Map<SomeEnum, Integer>          //map.get(SomeEnum.FOO) == 2
+ }
